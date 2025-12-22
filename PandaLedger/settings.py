f417d47af2471c2e791a41262dfd1de2491905e0
pandaa-69 <nytpanda1983@gmail.com>
2025-12-22 18:16:26 +0530

    Fix cors typo and move secretkey to env

--- PandaLedger/settings.py
+++ PandaLedger/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#4x()$rk)a3*k--!zn^0!hfrmj%+0$484m*#!(+rhb_)^$ek3o'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
@@ -71,7 +71,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",   # Only trust our React App
