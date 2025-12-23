00d6b58637316bd994435fbe5b6821b79254a0d3
pandaa-69 <nytpanda1983@gmail.com>
2025-12-24 03:37:47 +0530

    fix: final email settings for brevo port 2525

--- PandaLedger/settings.py
+++ PandaLedger/settings.py
@@ -148,9 +148,9 @@ AUTH_USER_MODEL = 'core.CustomUser'
 LOGIN_URL = '/api/auth/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = 'pandaledger.offcial@gmail.com'
